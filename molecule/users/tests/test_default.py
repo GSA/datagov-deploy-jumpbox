617d081db35d59e8c26f788dd93ef9bae550daa8
jbrown-xentity <jbrown@xentity.com>
2020-07-31 12:02:56 -0600

    remove invactive user test

--- molecule/users/tests/test_default.py
+++ molecule/users/tests/test_default.py
@@ -7,7 +7,6 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
 
 
 inactiveuser = 'inactiveuser'
-removeuser = 'removeuser'
 testuser = 'testuser'
 
 
@@ -17,14 +16,6 @@ def test_inactive_user(host):
 
     assert not passwd.contains(inactiveuser)
     assert not group.contains(inactiveuser)
-
-
-def test_removed_user(host):
-    passwd = host.file('/etc/passwd')
-    group = host.file('/etc/group')
-
-    assert not passwd.contains(removeuser)
-    assert not group.contains(removeuser)
 
 
 def test_active_user(host):
@@ -37,9 +28,7 @@ def test_active_user(host):
 
 def test_sudoers_removed(host):
     inactive_sudoer = host.file('/etc/sudoers.d/' + inactiveuser)
-    remove_sudoer = host.file('/etc/sudoers.d/' + removeuser)
     test_sudoer = host.file('/etc/sudoers.d/' + testuser)
 
     assert not inactive_sudoer.exists
-    assert not remove_sudoer.exists
     assert not test_sudoer.exists
